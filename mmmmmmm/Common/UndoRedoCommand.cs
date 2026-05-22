5172dc3f7d3ae324ed9845e63e3443b143ec4de4
lafro <94093072+lafro6978@users.noreply.github.com>
2026-05-23 03:22:38 +0700

    Add files via upload

---
+++ mmmmmmm/Common/UndoRedoCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mmmmmmmm.Common
+{
+    public class UndoRedoCommand
+    {
+        public Action UndoAction { get; }
+        public Action RedoAction { get; }
+
+        public UndoRedoCommand(Action undoAction, Action redoAction)
+        {
+            UndoAction = undoAction;
+            RedoAction = redoAction;
+        }
+    }
+}
---
+++ mmmmmmm/Common/UndoRedoManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mmmmmmmm.Common
+{
+    public class UndoRedoManager
+    {
+        private Stack<UndoRedoCommand> _undoStack = new Stack<UndoRedoCommand>();
+        private Stack<UndoRedoCommand> _redoStack = new Stack<UndoRedoCommand>();
+
+        public void AddUndoRedo(Action undoAction, Action redoAction)
+        {
+            var command = new UndoRedoCommand(undoAction, redoAction);
+            _undoStack.Push(command);
+            _redoStack.Clear();
+        }
+
+        public void Undo()
+        {
+            if (_undoStack.Count > 0)
+            {
+                var command = _undoStack.Pop();
+                command.UndoAction?.Invoke();
+                _redoStack.Push(command);
+            }
+        }
+
+        public void Redo()
+        {
+            if (_redoStack.Count > 0)
+            {
+                var command = _redoStack.Pop();
+                command.RedoAction?.Invoke();
+                _undoStack.Push(command);
+            }
+        }
+    }
+}
