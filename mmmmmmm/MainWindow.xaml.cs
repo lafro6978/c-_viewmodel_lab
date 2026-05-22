f5e032a0c0b209998b5f9668bcddd29115e2155d
lafro <94093072+lafro6978@users.noreply.github.com>
2026-05-23 03:16:40 +0700

    Add files via upload

---
+++ mmmmmmm/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using mmmmmmmm.ViewModel;
+
+namespace mmmmmmmm
+{
+    public partial class MainWindow : Window
+    {
+        private MainViewModel _mainViewModel;
+
+        public MainWindow()
+        {
+            _mainViewModel = new MainViewModel();
+            DataContext = _mainViewModel;
+            InitializeComponent();
+        } // <-- ВАЖНО: конструктор должен закрываться здесь!
+
+        // Все обработчики кнопок идут НИЖЕ конструктора:
+
+        private void UndoButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.Undo();
+        }
+
+        private void Redo_OnClick(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.Redo();
+        }
+
+        private void SelectFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.InputViewModel.SelectFile();
+        }
+
+        private void LoadData_OnClick(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.InputViewModel.LoadDataFromFile();
+            _mainViewModel.SolverViewModel.Solve();
+        }
+    }
+}
